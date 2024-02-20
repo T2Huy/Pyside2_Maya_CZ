d7a462520c1599418f54cff51df9ba8b8a45e6b0
T2Huy <truongquanghuy10a1@gmail.com>
2024-02-20 14:23:09 +0700

    update: dockable button

--- 10_Docking.py
+++ 10_Docking.py
@@ -1,14 +1,26 @@
 from PySide2 import QtWidgets
+from shiboken2 import getCppPointer
+
 from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
+from maya.OpenMayaUI import MQtUtil
+
 import maya.cmds as cmds
 
 class MyDockableButton(MayaQWidgetDockableMixin, QtWidgets.QPushButton):
 
-    def __init__(self):
-        super(MyDockableButton,self).__init__()
+    def __init__(self, workspace_control_name=None):
+        super(MyDockableButton, self).__init__()
 
         self.setWindowTitle("Dockable Window")
         self.setText("My Button")
+
+        if workspace_control_name:
+            workspace_control_ptr = MQtUtil.findControl(workspace_control_name)
+
+            if workspace_control_ptr is not None:
+                widget_ptr = int(getCppPointer(self)[0])
+
+                MQtUtil.addWidgetToMayaLayout(widget_ptr, int(workspace_control_ptr))
 
 if __name__ == "__main__":
 
@@ -23,4 +35,8 @@ if __name__ == "__main__":
         pass
 
     button = MyDockableButton()
-    button.show(dockable=True, uiScript="pass")
+
+    workspace_control_name = f"{button.objectName()}WorkspaceControl"
+    ui_script = f"from Pyside2_for_Maya.My_Dockable_button import MyDockableButton\nbutton = MyDockableButton('{workspace_control_name}')"
+
+    button.show(dockable=True, uiScript=ui_script)
