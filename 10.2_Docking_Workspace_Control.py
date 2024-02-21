c96834b8a604b62beab00c1a3477bf62c985d8aa
T2Huy <truongquanghuy10a1@gmail.com>
2024-02-21 16:05:10 +0700

    update: docking workspace control - add ui

--- 10.2_Docking_Workspace_Control.py
+++ 10.2_Docking_Workspace_Control.py
@@ -53,12 +53,70 @@ class WorkspaceControl(object):
     def is_collapsed(self):
         return cmds.workspaceControl(self.name, q=True, collapse=True)
 
+class SampleUI(QtWidgets.QWidget):
+
+    WINDOW_TITLE = "Sample UI"
+    UI_NAME = "SampleUI"
+    ui_instance = None
+
+    @classmethod
+    def display(cls):
+        if cls.ui_instance:
+            cls.ui_instance.show_workspace_control()
+        else:
+            cls.ui_instance = SampleUI()
+
+    @classmethod
+    def get_workspace_control_name(cls):
+        return f"{cls.UI_NAME}WorkspaceControl"
+
+    def __init__(self):
+        super(SampleUI, self).__init__()
+
+        self.setObjectName(self.__class__.UI_NAME)
+        self.setMinimumSize(200, 100)
+
+        self.create_widgets()
+        self.create_layout()
+        self.create_connections()
+        self.create_workspace_control()
+
+    def create_widgets(self):
+        self.apply_button = QtWidgets.QPushButton("Apply")
+
+    def create_layout(self):
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.setContentsMargins(2,2,2,2)
+        main_layout.addStretch()
+        main_layout.addWidget(self.apply_button)
+
+    def create_connections(self):
+        self.apply_button.clicked.connect(self.on_clicked)
+
+    def create_workspace_control(self):
+        self.workspace_control_instance = WorkspaceControl(self.get_workspace_control_name())
+        if self.workspace_control_instance.exists():
+            self.workspace_control_instance.restore(self)
+        else:
+            self.workspace_control_instance.create(self.WINDOW_TITLE, self, ui_script="from Pyside2_for_Maya.My_Dockable_button import SampleUI\nSampleUI.display()")
+
+    def show_workspace_control(self):
+        self.workspace_control_instance.set_visible(True)
+
+    def on_clicked(self):
+        print("Button Clicked")
+
 if __name__ == "__main__":
 
-    workspace_control_name = "MyWorkspaceControl"
-    if cmds.window(workspace_control_name, exists=True):
-        cmds.deleteUI(workspace_control_name)
+    # workspace_control_name = SampleUI.get_workspace_control_name()
+    # if cmds.window(workspace_control_name, exists=True):
+    #     cmds.deleteUI(workspace_control_name)
 
-    workspace_control = WorkspaceControl(workspace_control_name)
-    workspace_control.create(workspace_control_name, QtWidgets.QPushButton("Push Me!!!"))
-    workspace_control.set_label("Workspace Control")
+    try:
+        sample_ui.setParent(None)
+        sample_ui.deleteLater()
+    except:
+        pass
+
+    sample_ui = SampleUI()
+    sample_ui.show()
